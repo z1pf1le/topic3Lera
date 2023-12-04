bba105097021f0871681b9f7c467ec6435ca6b31
z1pf1le <total.zip.file@gmail.com>
2023-12-04 17:13:23 +0300

    получен список победителей

--- src/kosti/Main.java
+++ src/kosti/Main.java
@@ -21,6 +21,7 @@ public class Main{
         scanner.nextLine(); // Сброс новой строки после ввода числа
 
         ArrayList<String> listOfPlayers = new ArrayList<>();
+        ArrayList<String> listOfWinners = new ArrayList<>(); //динамический массив, в который отправятся победители
 
         // Ввод строк от пользователя
         for (int i = 0; i < players; i++) {
@@ -29,11 +30,47 @@ public class Main{
             listOfPlayers.add(input);
         }
         listOfPlayers.add("Computer"); //добавление Компьютера в конец списка
-        String firstWinner = DiceGame.playDiceGame(listOfPlayers, diceNumber);
-        System.out.println("первый победитель: " + firstWinner);
-        System.out.println(DiceGame.playDiceGame(listOfPlayers, diceNumber));
-        String secondWinner = DiceGame.playDiceGame(listOfPlayers, diceNumber);
-        System.out.println("второй победитель: " + secondWinner);
 
+        String firstWinner = DiceGame.playDiceGame(listOfPlayers, diceNumber); //ищу первого победителя
+        System.out.println("первый победитель: " + firstWinner);
+        ArrayList<String> listOfPlayers2 = MoveElementToFirst.moveElementToFirst(listOfPlayers, firstWinner);//получаю новый список
+        System.out.println("новый порядок игроков: " + listOfPlayers2);
+        listOfWinners.add(firstWinner);
+
+        String secondWinner = DiceGame.playDiceGame(listOfPlayers2, diceNumber);//ищу второго победителя
+        System.out.println("второй победитель: " + secondWinner);
+        ArrayList<String> listOfPlayers3 = MoveElementToFirst.moveElementToFirst(listOfPlayers2, firstWinner);//получаю новый список
+        System.out.println("новый порядок игроков: " + listOfPlayers3);
+        listOfWinners.add(secondWinner);
+
+        String thirdWinner = DiceGame.playDiceGame(listOfPlayers3, diceNumber);//ищу третьего победителя
+        System.out.println("третий победитель: " + thirdWinner);
+        ArrayList<String> listOfPlayers4 = MoveElementToFirst.moveElementToFirst(listOfPlayers3, firstWinner);//получаю новый список
+        System.out.println("новый порядок игроков: " + listOfPlayers4);
+        listOfWinners.add(thirdWinner);
+
+        String fourthWinner = DiceGame.playDiceGame(listOfPlayers4, diceNumber);//ищу четвертого победителя
+        System.out.println("четвертый победитель: " + fourthWinner);
+        ArrayList<String> listOfPlayers5 = MoveElementToFirst.moveElementToFirst(listOfPlayers4, firstWinner);//получаю новый список
+        System.out.println("новый порядок игроков: " + listOfPlayers5);
+        listOfWinners.add(fourthWinner);
+
+        String fifthWinner = DiceGame.playDiceGame(listOfPlayers5, diceNumber);//ищу пятого победителя
+        System.out.println("пятый победитель: " + fifthWinner);
+        ArrayList<String> listOfPlayers6 = MoveElementToFirst.moveElementToFirst(listOfPlayers4, firstWinner);//получаю новый список
+        System.out.println("новый порядок игроков: " + listOfPlayers6);
+        listOfWinners.add(fifthWinner);
+
+        String sixthWinner = DiceGame.playDiceGame(listOfPlayers6, diceNumber);//ищу шестого победителя
+        System.out.println("шестой победитель: " + sixthWinner);
+        ArrayList<String> listOfPlayers7 = MoveElementToFirst.moveElementToFirst(listOfPlayers4, firstWinner);//получаю новый список
+        System.out.println("новый порядок игроков: " + listOfPlayers7);
+        listOfWinners.add(sixthWinner);
+
+        String seventhWinner = DiceGame.playDiceGame(listOfPlayers7, diceNumber);//ищу седьмого победителя
+        System.out.println("седьмой победитель: " + seventhWinner);
+        listOfWinners.add(seventhWinner);
+
+        System.out.println(listOfWinners);
     }
 }
--- src/kosti/MoveElementToFirst.java
+++ src/kosti/MoveElementToFirst.java
@@ -1,34 +1,24 @@
 package kosti;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
 
 
 public class MoveElementToFirst {
-//    public static void main(String[] args) {
-//        String[] inputArray = {"первый", "второй", "третий", "четвертый"};
-//        String targetString = "третий";
-//
-//        String[] resultArray = moveElementToFirst(inputArray, targetString);
-//
-//        System.out.println(Arrays.toString(resultArray));
-//    }
 
-    public static String[] moveElementToFirst(String[] array, String target) {
-        if (array == null & array.length == 0) {
-            return array;
+
+    public static ArrayList<String> moveElementToFirst(ArrayList<String> list, String target) {
+        if (list == null | list.isEmpty()) {
+            return list;
         }
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(target)) {
-                // меняем местами элементы
-                String temp = array[0];
-                array[0] = array[i];
-                array[i] = temp;
-                break;
-            }
+        if (list.contains(target)) {
+            list.remove(target);
+            list.add(0, target);
         }
 
-        return array;
+        return list;
     }
 }
+
