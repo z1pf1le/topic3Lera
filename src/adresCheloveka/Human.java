ff534ce96956d2e4a05feb8e2ae213992f7711bc
z1pf1le <total.zip.file@gmail.com>
2023-12-04 17:55:05 +0300

    тема 3 задание 3 продолжение

--- src/adresCheloveka/Human.java
+++ src/adresCheloveka/Human.java
@@ -6,5 +6,11 @@ public class Human{
     int birthDate;
     Adress adress;
 
+    public Human(String firstName, String secondName, int birthDate, Adress adress) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.birthDate = birthDate;
+        this.adress = adress;
+    }
 }
 
--- src/adresCheloveka/Main.java
+++ src/adresCheloveka/Main.java
@@ -6,5 +6,10 @@ public class Main{
         Adress adress2 = new Adress(2, "Suzdal", "Pushkina", 3);
         Adress adress3 = new Adress(3, "Pokrov", "Lermontova", 42);
         Adress adress4 = new Adress(4, "Moscow", "Shevchenko", 24);
+
+        Human human1 = new Human("Ivan", "Petrov", 1989, adress1);
+        Human human2 = new Human("Petr", "Ivanov", 1988, adress2);
+        Human human3 = new Human("Maria", "Petrenko", 1990, adress3);
+        Human human4 = new Human("Ad", "Gain", 1991, adress4);
     }
 }
