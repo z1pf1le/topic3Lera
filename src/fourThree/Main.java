fe0c19faa3e7ab3df2b7976200a96adcd673b83d
z1pf1le <total.zip.file@gmail.com>
2023-12-09 13:36:32 +0300

    4-3.1

--- src/fourThree/Main.java
+++ src/fourThree/Main.java
@@ -3,6 +3,6 @@ package fourThree;
 public class Main{
     public static void main(String[] args) {
         int number = 12345;
-        System.out.println(ReverseNumber.separateDigits(number));
+        ReverseNumber.separateDigits(number);
     }
 }
--- src/fourThree/ReverseNumber.java
+++ src/fourThree/ReverseNumber.java
@@ -1,25 +1,25 @@
 package fourThree;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ReverseNumber {
-    public static boolean separateDigits(int number) {
+    public static Integer[] separateDigits(int number) {
         String numberStr = String.valueOf(number);
         Integer[] arr = new Integer[numberStr.length()];
         int j = numberStr.length();
+//        Integer result=0;
         for (int i = 0; i < numberStr.length(); i++) {
 //            for (int j = numberStr.length()-1; j > 0; j--){
 //                arr[j] = numberStr.charAt(i);
-
+                Integer preResult = i * numberStr.length();
                 arr[i] = j;
                 j--;
-
+//            result = result + preResult;
             System.out.println(numberStr.charAt(i));
         }
         System.out.println(Arrays.deepToString(arr));
-        return false;
-
+//        System.out.println(result);
+        return arr;
     }
 
 
